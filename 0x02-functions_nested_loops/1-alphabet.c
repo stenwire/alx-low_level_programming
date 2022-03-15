8436c47280684f4ea47cf5c18833bd0b42de7386
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-03-15 20:22:44 +0100

    C functions nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints alphabet -- Lower case.
+ *
+ * Return: Always 0.
+ */
+int print_alphabet(void)
+{
+	char Alphs;
+
+	for (Alphs = 'a'; Alphs <= 'z'; Alphs++)
+	{
+		_putchar(Alphs);
+	}
+
+	_putchar('\n');
+	return (0);
+}

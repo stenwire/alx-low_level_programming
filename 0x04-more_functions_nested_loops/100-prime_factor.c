242a1b6c3caccf8954e9b26126a7fc156e46d7b3
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-03-18 13:21:29 +0100

    C-Debuggin

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,34 +1,22 @@
-#include "main.h"
-/**
- * print_triangle - print a triangle
- * @size: the size of the triangle
- *
- * Return: a triangle
- */
-void print_triangle(int size)
-{
-	int a, b;
+#include <stdio.h>
 
-	if (size > 0)
-	{
-		for (a = 0 ; a < size ; a++)
-		{
-			for (b = 0 ; b < size ; b++)
-			{
-				if (b < size - (a + 1))
-				{
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(35);
-				}
-			}
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
+/**
+ * main - prints the highest prime factor of 612852475143
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	unsigned long int prime = 612852475143;
+	unsigned long int i = 3;
+
+	while (i <= prime / 2)
+		if (prime % i == 0)
+			prime = prime / i;
+		else
+			i += 2;
+
+	printf("%ld\n", prime);
+
+	return (0);
 }

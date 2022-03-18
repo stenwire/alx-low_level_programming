d5977abda80e6f1942cfdd4de7b2e344a4bb38f7
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-03-18 12:14:45 +0100

    More nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  * _isupper - checks for uppercase character
- *   * @c: the character to print
- *    *
- *     * Return: 1 or 0
- *      */
+ * _isupper - checks for uppercase character
+ * @c: the character to print
+ *
+ * Return: 1 or 0
+ */
 int _isupper(int c)
 {
 	if ((c >= 65) && (c <= 90))

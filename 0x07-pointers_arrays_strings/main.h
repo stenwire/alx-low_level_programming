670f44133d5892eabbf27c7a2d5d2cad050f8dd4
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-03-29 01:00:10 +0100

    More on pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+/**
+ * @file main.h
+ * @author stentechy (nwankwostephen039@yahoo.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+void print_diagsums(int *a, int size);
+
+void print_chessboard(char (*a)[8]);
+
+char *_strstr(char *haystack, char *needle);
+
+char *_strpbrk(char *s, char *accept);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strchr(char *s, char c);
+
+char *_memcpy(char *dest, char *src, unsigned int n);

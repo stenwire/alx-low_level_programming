8d179feca24d63f9453353ee8eddb29b87dd30cb
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-04-01 23:12:54 +0100

    C - argc and argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, __attribute__((unused)) char *argv[]);
+
+#endif /* HOLBERTON_H */

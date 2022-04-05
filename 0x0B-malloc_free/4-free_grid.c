0b2f438e7e464f14490cc1af9ca009ada85479a9
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-04-05 22:44:32 +0100

    fixed betty style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - Entry point
+ *@grid: rows of matrix
+ *@height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
+ */
 
 void free_grid(int **grid, int height)
 {

d635607fd7093f58d5126673f5bd2f579eab9636
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-04-11 15:34:43 +0100

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,5 +2,5 @@
 #include <stdlib.h>
 #include <stdlib.h>
 
-#define PRINTFILE() { char buf[] = __FILE__; printf("Filename:  %s\n", basename(buf)); }
+#define PRINTFILE() { char buf[] = __FILE__; printf("%s\n", basename(buf)); }
 

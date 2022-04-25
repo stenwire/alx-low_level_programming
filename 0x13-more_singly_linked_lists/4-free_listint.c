9e1af73dde04482f2eb5904c929cfcdbe045aff4
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-04-25 23:40:33 +0100

    function to free a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_listint - Entry Point
+ * @head: head
+ * Return: 0
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head)
+	{
+		free(head);
+		head = head->next;
+	}
+}

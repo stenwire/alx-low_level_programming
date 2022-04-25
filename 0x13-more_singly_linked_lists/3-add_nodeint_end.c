a739c7b3e06669545b2f22383ff7728d45483a6b
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-04-25 23:55:59 +0100

    Fix code and betty style

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,35 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * @brief 
- * 
- * @param head 
- * @param n 
- * @return listint_t* 
+ * add_nodeint_end - adds a new node at the end of a listint_t list.
+ * @head: double pointer to the beginning of the list
+ * @n: integer to add to the list
+ *
+ * Return: pointer to the new node
  */
-
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t* new_node = (listint_t*)malloc(sizeof(listint_t));
+	listint_t *new;
+	listint_t *current;
 
-    listint_t *last = *head;
-    new_node->n = n;
-    new_node->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return 0;
-    }
-
-    while (last->next != NULL)
-        last = last->next;
-
-
-    last->next = new_node;
-    return (new_node);
-}  
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new;
+	return (new);
+}

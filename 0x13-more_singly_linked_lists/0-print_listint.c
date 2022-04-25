e3a4e84cdffe2424767cfe5e59450f08bdf30219
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-04-25 23:11:03 +0100

    fix betty style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,22 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
 /**
- * @print_listint: function to print elements in a linked list
- * @h: pointer to the head 
- * @size_t: data type for function
+ * print_listint - Entry Point
+ * @h: head
+ * Return: 0
  */
-
 size_t print_listint(const listint_t *h)
 {
-    int count = 0;
-    while(h != NULL)
-    {
-        count++;
-        printf("%d\n", h->n);
-        h = h->next;
-}
-    return (count);
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
 

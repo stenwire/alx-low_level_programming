59d8369dbfc292fb502d734478be43c7831ed498
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-11 07:51:58 +0100

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,16 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Count number of elements in the list
- * @h: Head of the linked list
+ * dlistint_len - count the number of nodes in the linked list
+ * @h: pointer to the beginning of a linked list
  *
- * Return: Number of nodes (size_t)
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i = 0;
+	size_t i;
 
-	while (h != NULL)
-	{
-		i++;
+	for (i = 0; h != NULL; i++)
 		h = h->next;
-	}
 	return (i);
 }

7c49f96e52ef743ef22358cd9ba0e8569263f032
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-06-11 07:54:59 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,22 +1,20 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_dlistint - Free the list
- * @head: Head of the list
+ * free_dlistint - free a dlistint_t list
+ * @head: pointer to the beginning of the linked list
  *
- * Return: nothing
+ * Return: void
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp;
-
-	if (head == NULL)
-		return;
+	dlistint_t *next;
 
 	while (head != NULL)
 	{
-		temp = head;
-		head = temp->next;
-		free(temp);
+		next = head->next;
+		free(head);
+		head = next;
 	}
 }
